4a9e02ef173e113a81f42f2e0964cfeb88e0b94b
DuyTan.Pham (Stanley00) <duytan.pham.00@gmail.com>
2015-04-06 19:20:58 +0700

    Add support for GIFs

--- 9gagnsfw.js
+++ 9gagnsfw.js
@@ -2,9 +2,14 @@
 function genericOnClick(info, tab) {
 //  var s = tab.url
   var s = info.linkUrl
-  var f = s.substr(s.lastIndexOf('/') + 1)
-  var n = "http://d24w6bsrhbeh9d.cloudfront.net/photo/" + f + "_700b.jpg"
-  chrome.tabs.create({url: n})
+  if (s.indexOf("9gag.com")>-1)
+  {
+    var f = s.substr(s.lastIndexOf('/') + 1)
+    var n = "http://d24w6bsrhbeh9d.cloudfront.net/photo/" + f + "_700b.jpg"
+    var m = "http://img-9gag-ftw.9cache.com/photo/" + f + "_460sv.mp4"
+    chrome.tabs.create({url: n})
+    chrome.tabs.create({url: m})
+  }
 }
 
 // Create one test item for each context type.
